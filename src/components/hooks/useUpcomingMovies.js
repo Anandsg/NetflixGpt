80dd2630f00f59dcf9b32d79966d6cba1e3de580
Anandagadugin <gaduginanand984@gmail.com>
2024-02-24 16:28:38 +0530

    scrollbar fix

--- src/components/hooks/useUpcomingMovies.js
+++ src/components/hooks/useUpcomingMovies.js
@@ -4,18 +4,18 @@ import { API_OPTIONS } from "../../utils/constants";
 import { useEffect } from "react";
 
 const useUpcomingMovies = () => {
-    const dispatch = useDispatch();
-    const getUpcomingMovies = async () => {
-        const data = await
-            fetch('https://api.themoviedb.org/3/movie/upcoming?page=1',
-                API_OPTIONS
-            );
-        const json = await data.json();
-        dispatch(UpcomingMovies(json.results));
-    };
-    useEffect(() => {
-        getUpcomingMovies();
-    }, [])
+  const dispatch = useDispatch();
+  const getUpcomingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/upcoming?page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    dispatch(UpcomingMovies(json.results));
+  };
+  useEffect(() => {
+    getUpcomingMovies();
+  }, []);
 };
 
 export default useUpcomingMovies;
--- src/components/secondaryContainer/MovieList.js
+++ src/components/secondaryContainer/MovieList.js
@@ -1,43 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import MovieCard from './MovieCard';
-import './MovieList.css';
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import "./MovieList.css";
 
 const MovieList = ({ title, movies }) => {
-    const [style, setStyle] = useState({});
+  const [style, setStyle] = useState({});
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth >= 768) {
-                // Medium and large screens
-                setStyle({ top: '600px', position: 'relative' });
-            } else {
-                // Small screens
-                setStyle({});
-            }
-        };
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        // Medium and large screens
+        setStyle({ top: "600px", position: "relative" });
+      } else {
+        // Small screens
+        setStyle({});
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-    return (
-        <div className='py-2 p-4 bg-black' style={style}>
-            <h1 className='text-md md:text-2xl font-semibold p-2 md:p-2 text-white'>{title}</h1>
-            <div className='flex p-2 flex-row overflow-x-scroll overflow-hidden scrollbar'>
-                <div className='flex'>
-                    {movies && movies.length > 0 ? (
-                        movies.map((movie, index) => (
-                            <MovieCard id={movie?.id} posterPath={movie?.backdrop_path} key={index} title={movie?.original_title} />
-                        ))
-                    ) : (
-                        <p>No movies to display</p>
-                    )}
-                </div>
-            </div>
+  return (
+    <div className="py-2 p-4 bg-black" style={style}>
+      <h1 className="text-md md:text-2xl font-semibold p-2 md:p-2 text-white">
+        {title}
+      </h1>
+      <div className="flex p-2 flex-row overflow-x-scroll overflow-hidden scrollbar">
+        <div className="flex">
+          {movies && movies.length > 0 ? (
+            movies.map((movie, index) => (
+              <MovieCard
+                id={movie?.id}
+                posterPath={movie?.backdrop_path}
+                key={index}
+                title={movie?.original_title}
+              />
+            ))
+          ) : (
+            <p>No movies to display</p>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MovieList;
